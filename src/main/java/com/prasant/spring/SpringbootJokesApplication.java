b0abf7f2217799f7001b1341a4b84d0e04387166
Prasant Samal <samal_prasant123@yahoo.co.in>
2018-05-09 23:20:01 -0400

    Spring XML Configuration Example

--- src/main/java/com/prasant/spring/SpringbootJokesApplication.java
+++ src/main/java/com/prasant/spring/SpringbootJokesApplication.java
@@ -3,10 +3,12 @@ package com.prasant.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
 import com.prasant.spring.services.JokesServiceImpl;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class SpringbootJokesApplication {
 
 	public static void main(String[] args) {
